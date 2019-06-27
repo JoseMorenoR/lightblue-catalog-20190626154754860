231a07fd51a221b58eb3043166aea771031bcf20
Jose Luis Moreno <jose.moreno@docsolutions.com>
2019-06-27 18:58:07 +0000

    Codigo repetido

--- src/main/java/catalog/models/InventoryRepo.java
+++ src/main/java/catalog/models/InventoryRepo.java
@@ -14,20 +14,4 @@ import org.springframework.stereotype.Repository;
 @Repository("itemsRepo")
 @Transactional
 public interface InventoryRepo extends CrudRepository<Inventory, Long> {
-}package catalog.models;
-
-import java.util.List;
-import javax.transaction.Transactional;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-/**
- * Orders Repository
- *
- */
-
-@Repository("itemsRepo")
-@Transactional
-public interface InventoryRepo extends CrudRepository<Inventory, Long> {
 }
